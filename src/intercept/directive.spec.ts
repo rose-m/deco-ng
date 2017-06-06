090ddbd1f9cf85d3b3d0672e765a387cb94e88a7
rosem <michael_rose@gmx.de>
2017-06-06 18:50:51 +0200

    more tests

--- src/intercept/directive.spec.ts
+++ src/intercept/directive.spec.ts
@@ -1,7 +1,16 @@
 describe('intercept.directive', () => {
+    /**
+     * Setting up module to use for directive testing
+     * @type {angular.IModule}
+     */
     const testModule = angular.module('test', [])
-        .directive('simpleDirective', simpleDirective);
+        .directive('simpleDirective', simpleDirective)
+        .directive('simpleDirectiveWithInjection', simpleDirectiveWithInjection)
+        .directive('simpleDirectiveWithMinifiedInjection', ['$rootScope', simpleDirectiveWithInjection]);
 
+    // ==============================
+    // Starting test implementation
+    // ==============================
     let $compile: ng.ICompileService, $rootScope: ng.IRootScopeService;
 
     beforeEach(angular.mock.module('test'));
@@ -14,10 +23,30 @@ describe('intercept.directive', () => {
         expect(testModule).toBeDefined();
     });
 
-    it('does not interfere with simple directive', () => {
+    it('does not interfere with simple directive without injection', () => {
         const element = $compile(`<simple-directive variable="just testing"></simple-directive>`)($rootScope);
         $rootScope.$digest();
         expect(element.html()).toContain('just testing');
+    });
+
+    it('does not interfere with simple directive with injection', () => {
+        const spy = jasmine.createSpy('testEventHandler');
+        $rootScope.$on('testevent', spy);
+
+        const element = $compile(`<simple-directive-with-injection variable="just testing"></simple-directive-with-injection>`)($rootScope);
+        $rootScope.$digest();
+        expect(element.html()).toContain('just testing');
+        expect(spy.calls.mostRecent().args[1]).toEqual('test data');
+    });
+
+    it('does not interfere with simple directive with injection when minifaction safe', () => {
+        const spy = jasmine.createSpy('testEventHandler');
+        $rootScope.$on('testevent', spy);
+
+        const element = $compile(`<simple-directive-with-minified-injection variable="just testing"></simple-directive-with-minified-injection>`)($rootScope);
+        $rootScope.$digest();
+        expect(element.html()).toContain('just testing');
+        expect(spy.calls.mostRecent().args[1]).toEqual('test data');
     });
 });
 
@@ -27,6 +56,19 @@ function simpleDirective(): ng.IDirective {
         template: `<div>{{variable}}</div>`,
         scope: {
             variable: '@'
+        }
+    };
+}
+
+function simpleDirectiveWithInjection($rootScope: ng.IRootScopeService): ng.IDirective {
+    return {
+        restrict: 'E',
+        template: `<div>{{variable}}</div>`,
+        scope: {
+            variable: '@'
+        },
+        link: () => {
+            $rootScope.$emit('testevent', 'test data');
         }
     };
 }
